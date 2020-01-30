fedf9561a2fbabb27e7cf2e78a770b542103264d
Kayla Dailey <kayladailey000@gmail.com>
2020-01-29 21:58:36 -0600

    Initial commit and file clean up CRA

---
+++ tutorial/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className="App">
+   <h1>State & Props</h1>
+   <h3>The Need to Know</h3>
+    </div>
+  );
+}
+
+export default App;
