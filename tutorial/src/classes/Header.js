6590e90688c37b008f4b3c4d4f9aed4803260c4c
Kayla Dailey <kayladailey000@gmail.com>
2020-01-30 21:50:16 -0600

    notes for event handlers

--- tutorial/src/classes/Header.js
+++ tutorial/src/classes/Header.js
@@ -14,3 +14,26 @@ export default class Header extends React.Component {
     );
   }
 }
+
+
+//Input onclick handlers
+  
+// import React from "react";
+
+// import Title from "./Header/Title";
+
+// export default class Header extends React.Component {
+//   handleChange(e) {
+//     const title = e.target.value;
+//     this.props.changeTitle(title);
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <Title title={this.props.title} />
+//         <input value={this.props.title} onChange={this.handleChange.bind(this)} />
+//       </div>
+//     );
+//   }
+// }
